7fdeb4aff3ef1d6c928c63d839b2b17015ec3f42
Hugofernandezfpm <hugofernandez.fpb@gmail.com>
2023-09-27 13:37:57 +0200

    ejercicios

---
+++ Ejercicios/Ejercicio4boletin3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4boletin3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe valor de radio:"); //Para que te pida que escribas el valor de radio
+            double radio  = double.Parse(Console.ReadLine()); //Para que tu mismo le des el valor a radio
+            double longitud, area, volumen, PI; 
+            PI = Math.PI;
+           longitud = 2 * PI * radio;
+            area = PI * radio * radio;
+            volumen = (4f / 3) * PI * radio * radio * radio; //hay q ponerle la f para que cuente todos los decimales
+
+            
+            Console.WriteLine("Longitud =" + longitud);
+            Console.WriteLine("area =" + area);
+            Console.WriteLine("volumen =" + volumen);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicios/ejercicio7boletin3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio7boletin3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit;
+            Console.WriteLine("Introduce grados celsius: ");
+            celsius = double.Parse(Console.ReadLine());  //tambien se puede hacer con Convert.ToDouble
+
+            fahrenheit = celsius * 9 / 5 + 32;
+            Console.WriteLine("Grados Fahrenheit: " + fahrenheit);
+            Console.ReadLine();
+        }
+    }
+}
